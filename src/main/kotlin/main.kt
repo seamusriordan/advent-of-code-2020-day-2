f45ed93e432d0dee9021a82a24fbd0a97c04723a
Seamus Riordan <seamus@riordan.io>
2020-12-03 06:51:57 -0600

    Conformance tests and tests for examples

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,10 @@ fun main(args: Array<String>) {
     println("Hello World!")
 }
 
+fun doesPasswordConform(password: String, policy: PasswordPolicy): Boolean {
+    val instances = password.toCharArray().filter { char -> char == policy.policyCharacter}.size
+    return instances >= policy.lowerBound && instances <= policy.upperBound
+}
 
 fun extractPassword(input: String): String {
     return PasswordPolicy.extractInputGroups(input)[4]
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -2,8 +2,6 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class MainTest {
-
-
     @Test
     internal fun `1-3 a abcde extracts password abcde`() {
         val input = "1-3 a: abcde"
@@ -13,7 +11,6 @@ class MainTest {
         assertEquals("abcde", password)
     }
 
-
     @Test
     internal fun `2-4 d abrdz extracts password abrdz`() {
         val input = "2-4 d: abrdz"
@@ -21,5 +18,79 @@ class MainTest {
         val password = extractPassword(input)
 
         assertEquals("abrdz", password)
+    }
+
+    @Test
+    internal fun `doesPasswordConform with one char equal to lower bounds passes`() {
+        val policy = PasswordPolicy().apply {
+            lowerBound = 1
+            upperBound = 1
+            policyCharacter = 'a'
+        }
+
+        assertEquals(true, doesPasswordConform("a", policy))
+    }
+
+    @Test
+    internal fun `doesPasswordConform with one char below lower bounds fails`() {
+        val policy = PasswordPolicy().apply {
+            lowerBound = 2
+            upperBound = 2
+            policyCharacter = 'a'
+        }
+
+        assertEquals(false, doesPasswordConform("a", policy))
+    }
+
+    @Test
+    internal fun `doesPasswordConform with one char above upper bounds fails`() {
+        val policy = PasswordPolicy().apply {
+            lowerBound = 2
+            upperBound = 2
+            policyCharacter = 'a'
+        }
+
+        assertEquals(false, doesPasswordConform("aaa", policy))
+    }
+
+    @Test
+    internal fun `doesPasswordConform with two char within bounds passes`() {
+        val policy = PasswordPolicy().apply {
+            lowerBound = 1
+            upperBound = 1
+            policyCharacter = 'a'
+        }
+
+        assertEquals(true, doesPasswordConform("ba", policy))
+    }
+
+    @Test
+    internal fun `doesPasswordConform 1-3 a abcde example passes`() {
+        val input = "1-3 a: abcde"
+
+        val policy = PasswordPolicy.build(input)
+        val password = extractPassword(input)
+
+        assertEquals(true, doesPasswordConform(password, policy))
+    }
+
+    @Test
+    internal fun `doesPasswordConform 1-3 b cdefg example fails`() {
+        val input = "1-3 b: cdefg"
+
+        val policy = PasswordPolicy.build(input)
+        val password = extractPassword(input)
+
+        assertEquals(false, doesPasswordConform(password, policy))
+    }
+
+    @Test
+    internal fun `doesPasswordConform 2-9 c ccccccccc example passes`() {
+        val input = "2-9 c: ccccccccc"
+
+        val policy = PasswordPolicy.build(input)
+        val password = extractPassword(input)
+
+        assertEquals(true, doesPasswordConform(password, policy))
     }
 }
